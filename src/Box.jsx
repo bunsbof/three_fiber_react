b1cc002bff94f43e39d03a196b0a817a8e2d7d2b
The Untitled <nguyenminhcuong2000@gmail.com>
2023-04-15 22:08:48 +0700

    working with events in react three

--- src/Box.jsx
+++ src/Box.jsx
@@ -1,6 +1,20 @@
-export function Box() {
+import { useRef } from "react";
+import { useFrame } from "@react-three/fiber";
+
+export function Box(props) {
+  const ref = useRef();
+  useFrame((_, delta) => {
+    ref.current.rotation.x += 1 * delta;
+    ref.current.rotation.y += 0.5 * delta;
+  });
   return (
-    <mesh>
+    <mesh
+      {...props}
+      ref={ref}
+      onPointerUp={(e) => console.log("pointer up" + e.object.name)}
+      onPointerDown={(e) => console.log("pointer down" + e.object.name)}
+      onPointerOver={(e) => console.log("ponter over" + e.object.name)}
+    >
       <boxGeometry />
       <meshBasicMaterial color={0x00ff00} wireframe />
     </mesh>
